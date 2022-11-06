5f83fc47680d3f50c6406ea4f60d189f0bcb2a0e
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@gmail.com>
2022-11-06 15:10:36 +0100

    Added Resource struct

--- shared/domain/resource.go
+++ shared/domain/resource.go
@@ -1,5 +1,38 @@
 package domain
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/ansel1/merry"
+	"github.com/google/uuid"
+)
+
+type Updateable struct {
+	UpdateAt time.Time `json:"updatedAt"`
+}
+
+type Deleteable struct {
+	DeletedAt time.Time `json:"deletedAt"`
+}
+
 type Resource struct {
-	ID string `json:"id"`
+	ID        string    `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func NewResource(id string, createdAt time.Time) (*Resource, error) {
+	if id == "" {
+		id = uuid.New().String()
+	} else {
+		_, err := uuid.Parse(id)
+		if err != nil {
+			return nil, merry.New(fmt.Sprintf("Invalid field ID: it must be a valid uuid. The value received is '%s'.", id))
+		}
+	}
+
+	return &Resource{
+		ID:        id,
+		CreatedAt: createdAt,
+	}, nil
 }
