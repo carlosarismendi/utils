a468c35d904770dcd9c84682a709dfbcbb55b2a2
Carlos Eduardo Arismendi Sanchez <carlos.arismendisanchez@gmail.com>
2022-11-13 17:55:47 +0100

    Removed shared/domain/resource

--- shared/domain/resource.go
+++ shared/domain/resource.go
@@ -1,38 +1,17 @@
 package domain
 
 import (
-	"fmt"
 	"time"
-
-	"github.com/ansel1/merry"
-	"github.com/google/uuid"
 )
 
-type Updateable struct {
-	UpdateAt time.Time `json:"updatedAt"`
-}
-
-type Deleteable struct {
-	DeletedAt time.Time `json:"deletedAt"`
-}
-
-type Resource struct {
-	ID        string    `json:"id"`
+type CreatedAt struct {
 	CreatedAt time.Time `json:"createdAt"`
 }
 
-func NewResource(id string, createdAt time.Time) (*Resource, error) {
-	if id == "" {
-		id = uuid.New().String()
-	} else {
-		_, err := uuid.Parse(id)
-		if err != nil {
-			return nil, merry.New(fmt.Sprintf("Invalid field ID: it must be a valid uuid. The value received is '%s'.", id))
-		}
-	}
+type UpdatedAt struct {
+	UpdateAt time.Time `json:"updatedAt"`
+}
 
-	return &Resource{
-		ID:        id,
-		CreatedAt: createdAt,
-	}, nil
+type DeletedAt struct {
+	DeletedAt time.Time `json:"deletedAt"`
 }
